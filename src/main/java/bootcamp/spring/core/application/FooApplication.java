1676210abce3de26554db239c065f3053b2d5923
wclouds19 <wan.essentials@gmail.com>
2023-06-13 14:41:35 +0700

    Adding Customizing Spring Application & Event

--- src/main/java/bootcamp/spring/core/application/FooApplication.java
+++ src/main/java/bootcamp/spring/core/application/FooApplication.java
@@ -1,11 +1,15 @@
 package bootcamp.spring.core.application;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import bootcamp.spring.core.data.Foo;
+import bootcamp.spring.core.listener.AppStartingListener;
 
 @SpringBootApplication
 public class FooApplication {
@@ -15,11 +19,26 @@ public class FooApplication {
         return new Foo();
     }
 
-    public static void main(String[] args){
+    //DEFAULT SPRING APPLICATION
+    /*public static void main(String[] args){
         ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
 
         Foo foo = applicationContext.getBean(Foo.class);
 
         System.out.println(foo);
+    }*/
+
+    //CUSTOMIZING SPRING APPLICATION
+    public static void main(String[] args){
+        SpringApplication application = new SpringApplication(FooApplication.class); 
+        application.setBannerMode(Mode.OFF);
+        application.setListeners(List.of(new AppStartingListener()));
+
+        ConfigurableApplicationContext applicationContext = application.run(args);
+
+        Foo foo = applicationContext.getBean(Foo.class);
+
+        System.out.println(foo);
     }
+
 }
